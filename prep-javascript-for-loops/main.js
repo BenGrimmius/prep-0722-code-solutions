89eeee3a2ba6e5811eb790f1ce9396f0ffcb0a4e
Ben Grimmius <bengrimmius@gmail.com>
2022-08-10 20:42:08 +0000

    Complete prep-javascript-for-loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+
+for (let i = 0; i < 10; i++) {
+  console.log('Ex 1', i);
+}
+
+for (let i = 0; i < 20; i = i + 2) {
+  console.log('Ex 2', i);
+}
+
+for (let i = 100; i; i--) {
+  var str2 = 'Time till explosion: ' + i;
+  console.log(str2);
+}
